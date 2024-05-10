37309eb760c455314ab00f00087ae9a91e08353c
DODANGHOANCNTT2K15 <dodanghoana12017@gmail.com>
2024-05-10 10:49:58 +0700

    Bug

---
+++ Bug.java
@@ -0,0 +1,9 @@
+public class Bug {
+    public void BugEx(int a, int b) {
+        if (a = b) {
+            System.out.println("a = b");
+        } else {
+            System.out.println("a != b");
+        }
+    }
+}
